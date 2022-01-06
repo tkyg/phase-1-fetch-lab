9351547ac72bdd568ac56d7a4d7a70ba2cc21217
tkyg <80114433+tkyg@users.noreply.github.com>
2022-01-06 12:35:12 -0500

    commit tree 0158c301ea5bfdd3fa81980563617f8608337804

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
 
+function fetchBooks() {
+  return fetch ('https://anapioficeandfire.com/api/books')
+  .then((resp) => resp.json()) 
+  .then ((json) => renderBooks(json))
+}
+// To pass the tests, don't forget to return your fetch!
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
